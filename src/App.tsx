cdaeebf202378593a46386a00e172b775748eeef
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-12 22:07:42 -0300

    criado pasta components  styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import './styles/theme.css'
+import './styles/global.css'
+
+import { Heading } from './components/Heading';
+
 export function App(){
     
     return (
         <>
-            <h1>Olá mundo!</h1>;
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iure veritatis enim sequi assumenda. Suscipit blanditiis officiis ut enim, quisquam sunt, illo dolor molestiae, nulla nam explicabo beatae hic illum.</p>
+            <Heading/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iure veritatis enim sequi assumenda. Suscipit blanditiis officiis ut enim, quisquam sunt, illo dolor molestiae, nulla nam explicabo beatae hic illum.
+
+            </p>
         </>
         
-    )
+    );
 }
