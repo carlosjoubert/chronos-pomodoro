8f5f51a67d1546984e24849bee52408f3ef8cc72
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-13 11:40:22 -0300

    criado a divisão da pagina (logo, menu, form, footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import './styles/theme.css'
 import './styles/global.css'
 
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 export function App() {
-
-    return (
-        <>
-            <Heading>
-                Olá mundo!
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iure veritatis enim sequi assumenda. Suscipit blanditiis officiis ut enim, quisquam sunt, illo dolor molestiae, nulla nam explicabo beatae hic illum.
-
-            </p>
-        </>
-
-    );
+    return <>
+        <div className='container'>
+            <div className='container-fluid'>
+                <div className='content'>
+                    <section>
+                        Logo
+                    </section>
+                </div>
+            </div>
+        </div >
+        <div className='container'>
+            <div className='container-fluid'>
+                <div className='content'>
+                    <section>
+                        Menu
+                    </section>
+                </div>
+            </div>
+        </div >
+        <div className='container'>
+            <div className='container-fluid'>
+                <div className='content'>
+                    <section>
+                        Form
+                    </section>
+                </div>
+            </div>
+        </div >
+        <div className='container'>
+            <div className='container-fluid'>
+                <div className='content'>
+                    <section>
+                        Footer
+                    </section>
+                </div>
+            </div>
+        </div >
+    </>
 }
