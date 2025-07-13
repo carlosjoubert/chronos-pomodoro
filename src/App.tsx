3ea8b7715fe7a6382a5c4453abd19a07c7ab280e
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-13 11:09:43 -0300

    cosntrução do heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import './styles/theme.css'
 import './styles/global.css'
 
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
-export function App(){
-    
+export function App() {
+
     return (
         <>
-            <Heading>Olá mundo!</Heading>
+            <Heading>
+                Olá mundo!
+                <button>
+                    <TimerIcon/>
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic iure veritatis enim sequi assumenda. Suscipit blanditiis officiis ut enim, quisquam sunt, illo dolor molestiae, nulla nam explicabo beatae hic illum.
 
             </p>
         </>
-        
+
     );
 }
