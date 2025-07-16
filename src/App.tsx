d1f3fc9cd02f477d003db08d7347997cacf39483
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-16 17:34:59 -0300

    criado componente DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,39 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaulfInput';
 
 export function App() {
     return (
         <>
             <Container>
-                <Logo/>
+                <Logo />
             </Container>
 
             <Container>
-                <Menu/>
+                <Menu />
             </Container>
             <Container>
-                <CountDown/>
+                <CountDown />
             </Container>
-             <Container>
+            <Container>
                 <form className='form' action="">
                     <div className="formRow">
-                        <label htmlFor="input">task</label>
-                        <input id='input' type="text" />
+                        <DefaultInput id='input' type='text' />
                     </div>
-                     <div className="formRow">
+                    <div className="formRow">
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
-                     <div className="formRow">
+                    <div className="formRow">
                         <p>Ciclos</p>
                         <p>0 0 0 0 0 0 0</p>
                     </div>
-                     <div className="formRow">
+                    <div className="formRow">
                         <button>Enviar</button>
                     </div>
                 </form>
             </Container>
-            
+
         </>
     );
 }
