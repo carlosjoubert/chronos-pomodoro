51323525e732c6e088f915a974c9cc6bc358e6e0
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-18 16:23:36 -0300

    craido componente cycles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaulfInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
     return (
@@ -29,8 +30,7 @@ export function App() {
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
                     <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0 0</p>
+                        <Cycles/>
                     </div>
                     <div className="formRow">
                         <button>Enviar</button>
