4238576ad2dff21a3aa9adf03702f126f6c32f7a
Carlos Joubert <carlosjoubert@gmail.com>
2025-07-19 22:26:30 -0300

    criado pasta contexts

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,11 @@ import './styles/theme.css'
 import './styles/global.css'
 
 import { Home } from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
-import { useState } from 'react';
-import { TaskContextProvider } from './contexts/TaskContext';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 
-
-const initialState: TaskStateModel = {
-    tasks: [],
-    secondsRemaining: 0,
-    formattedSecondsRemaining: '00:00',
-    activeTask: null,
-    currentCycle: 0,
-    config: {
-        wokTime: 25,
-        shortBreakTime: 5,
-        longBreakTime: 15,
-    }
-}
 
 export function App() {
-    const [state, setState] = useState<TaskStateModel>(initialState);
-
-
-
+   
     return (
         <TaskContextProvider>
             <Home />
